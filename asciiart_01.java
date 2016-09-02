96718479b6a2e8772bad33890a41f3e3b1b16cde
bradycoulter <CoulterB9565@sduhsd.lan>
2016-09-02 14:21:10 -0700

    Asciiartpart1
    Lmao, I still have one more ascii art to do.

---
+++ asciiart_01.java
@@ -0,0 +1,52 @@
+public class asciiart_01
+{
+	public static void main(String[]args)
+	{
+		System.out.println("XXXXXXXXXXXXXXXXXXNXXXXXXXXXXX0OkxxxxxxxxkO0KXXXXXNXXNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXX0kdolcccccccccccclodxk0XXXXXXXXXXX0Okxddoooooodxxk0XXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXX0xolcclllllllllllllllllcclodxOXXKOkdolcccllllllllllccldOXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXOdlclllllllllllllllllllllllllcccoolcccllllllllllllllllllccoOXXXXXXXXXXXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXXXXXX0dccllllllllllllllllllllllllllllllc::lllllllllllllllllllllllcckXXXXXXXXXXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXXXXKxlclllllllllllllcccc::::cccccclllllll::lllllllllllllllllllllllcckXXXXXXXXXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXXX0dclllllllllllcc::::cccccccccc::::::ccll::lllllllllllllllllllllllclOXXXXXXXXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXXOlclllllllllc:::cclllllllllllllllllcc:::::;:c:::::::cccccccc:::::cc:o0XXXNXXXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXOlclllllllllcccllllllllllllllllllllllllllc:,;:cccclllllllllllllcccc::clodxOKXXXNXXXXXX");
+		System.out.println("XXXXXXXXXXXXXOlclllllllllllllllllllllllllllllcccccccccccc:;;:clllllllllllllllllllllllllccldOKXXXXXXX");
+		System.out.println("XXXXXXXXXXXX0lclllllllllllllllllllllllccc::c::::::::::::::::;;:cllllllccc:::cc:::::cc::c::::lOXXXXXX");
+		System.out.println("XXXXXXXXXXKOoclllllllllllllllllllllc:::::::::::::::::::::c::c:::;:c::::::::::::::::::::::;;;:cokKXXX");
+		System.out.println("XXXXXXX0xdl:;clllllllllllllllllcc:::cc:::::cccccloodxkxxdoc:::::;;;::::cc::ccc:;;,,;:lllc:::::;;dKXX");
+		System.out.println("XXXXX0dlcll::lllllllllllllllc:::::::::coxxl;:c'.....:kNWWXK0Okxol::clllodxko;.::.   .,dKK0Odcc:;:d0X");
+		System.out.println("XXXXklclllc::llllllllllllc::::cc:::ldOXWXl. .;. '::. .oNMMMMMMWNXklcoxOXNW0,  .' .:c. .xWMMWX0Oko:oK");
+		System.out.println("XXKxccllllc:clllllllllll:;:::cccldOXWMMMO.   .. ;oo,  .OMMMMMMMMMNxxKWMMMWd.  .. .;c.  ;KMMMMMMMNkd0");
+		System.out.println("XKdcclllllcclllllllllllllc::c:::oKWMMMMMk.   .        .kMMMMMMMMMKONMMMMMWl            ;KMMMMMMMNkd0");
+		System.out.println("KxccllllllllllllllllllllllllllllcoxOKWMM0;           .oXMMMMMMWXOdOWMMMMMWO'          ,kWMWWXKOxddOX");
+		System.out.println("xcclllllllllllllllllllllllllllllccccldxO0kc'.. ....,lONWNXK0Okdlc;cdxkkO0KKx:..     .;okkxdolc::o0XX");
+		System.out.println("ccllllllllllllllllllllllllllllllc::::::cclooc::::clodxxdolcccccc::clccccccllolc;;;;:cccccllccoxOKXXX");
+		System.out.println("clllllllllllllllllllllllllllllllllllcc:::::::::::::::::ccc:::::cclllllllllllllllllllllllllclkXXXXXXX");
+		System.out.println("llllllllllllllllllllllllllllllllllllllllllccccccccccccc::::clllllllllllllllllllllllllclllox0XXXXXXXX");
+		System.out.println("clllllllllllllllllllllllllllllllllllllllllllllllllccc:::ccclllllllllc::::::ccc:::c::::oOKXXXXXXXNXXX");
+		System.out.println("cllllllllllllllllllllllllllllllllllllllllllllc::::::cclllllllllllllllllcc:;;:c:ccccllccdOXXXXXXXXXXX");
+		System.out.println("cllllllllllllllllllllllllllllllllllllllllllllccclllllllllllllllllllllllllllllllllllllllccd0XXXXXXXXX");
+		System.out.println("cllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllclOXXXXXXXX");
+		System.out.println("llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllco0XXXXXXX");
+		System.out.println("lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllcdXXXXXXX");
+		System.out.println("llllllllllllllllllllllllllllllcccccccccllllllllllllllllllllllllllllllllllllllllllllllllllllclOXXXXXX");
+		System.out.println("lllllllllllllllllllllllllllc:::::::::::::::::::ccccllllllllllllllllllllllllllllllllllllcc::::cxKXXXX");
+		System.out.println("lllllllllllllllllllllllllc;;:ccccccccccccccc::::::::::::::::cccccllllllllllllllccc:::::::::cc:lOXXXX");
+		System.out.println("llllllllllllllllllllllllc;:ccccc:::::::::::ccccccccccccc:::::::::::::::::::::::::::::ccccccc:cxKXXXX");
+		System.out.println("llllllllllllllllllllllll:;ccccccccccccc:::::::::::::::::cccccccccccccccccccccccccccccc::::cox0XXXXXX");
+		System.out.println("llllllllllllllllllllllllc:;:::ccccccccccccccccccc::::::::::::::::::::::::::::::::::::::::;l0NXXXXXXX");
+		System.out.println("llllllllllllllllllllc:clllc:::::::::::::::::::::ccccccccccccccccccccccccccccccccccccccccc:l0XXXNXXXX");
+		System.out.println("cllllllllllllllllllllc::clllllllllllllllllccc::::::::::::::::::::ccccccccccccccccccccc::coOXXXXXXXXX");
+		System.out.println("xlcllllllllllllllllllllc:::clllllllllllllllllllllllllcccccc:::::::::::::::::::::;;cooddk0XXXXXXXXXXX");
+		System.out.println("o:,::ccllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllcccokKXXXXXXXXXXXXXXXXX");
+		System.out.println("''',,;::::ccllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllcccoxOKXXXXXXXXXXNXXXXXXXX");
+		System.out.println(",,,,,,,;;;::::::ccclllllllllllllllllllllllllllllllllllllllllllllllllllcccodk0XXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println(",,,,,,,,',,;;:::::c:::::::::cccccclllllllllllllllllllllllllllllcccclodxk0KXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println(",,,,,,,,,,,,,,,,;;::cccccccccc::ccc::::::::::::::::::::::::::::,'';xKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println(",,,,,,,,,,,,,,,,,'',,,,;;;;;:::::::cccccccccccccccccccc:::::;;,,,,,;cdOKXXXXXXNXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'''',,,,,,,,,,,,,,,,,,,,,,'',,,,,,,,,,,:okKXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,ckKXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,';xXXXXXXXXXXXXXXXXXXXXXXX");		
+	}	
+}
---
+++ firstProg.java
@@ -0,0 +1,19 @@
+public class firstProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  ____                _       ");
+		System.out.println(" |  _ \              | |      ");
+		System.out.println("BB       BBB");
+		System.out.println("BB          BBB");
+		System.out.println("BB              BBB");
+		System.out.println("BB");
+		System.out.println("BB");
+		System.out.println("BB");
+		System.out.println("BB");
+		System.out.println("BB");
+		System.out.println("BB");
+		System.out.println("BB");
+		System.out.println("BB");
+	}
+}
