6025c77619435572594b63718cafe9379b819fbb
bradycoulter <CoulterB9565@sduhsd.lan>
2016-09-09 13:24:50 -0700

    Reorganization+ added asciiart_02
    Moved files into folders based on lesson

---
+++ AsciiArt_lesson_1/asciiart_02.java
@@ -0,0 +1,14 @@
+public class asciiart_02
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" ____                _       ");
+		System.out.println("|  _ \\              | |      ");
+		System.out.println("| |_) |_ __ __ _  __| |_   _ ");
+		System.out.println("|  _ <| '__/ _` |/ _` | | | |");
+		System.out.println("| |_) | | | (_| | (_| | |_| |");
+		System.out.println("|____/|_|  \\__,_|\\__,_|\\__, |");
+		System.out.println("                        __/ |");
+		System.out.println("                       |___/ ");
+	}	
+}
