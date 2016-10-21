8094f8d4e243de627761bb58d065685ecb1b2f31
bradycoulter <brady.coulter@gmail.com>
2016-10-21 13:50:42 -0700

    Restored compound interest lab
    compound interest lab had temporary sync problem

--- Lesson_4/compound_interest.java
+++ Lesson_4/compound_interest.java
@@ -5,6 +5,7 @@ public class compound_interest
 	{
 		Scanner kb = new Scanner(System.in);
 		compound_interest calcC = new compound_interest();
+		compound_interest form = new compound_interest();
 		System.out.println("Please enter the rate.");
 		double rate = kb.nextDouble();
 		System.out.println("Please enter the principal.");
@@ -14,7 +15,7 @@ public class compound_interest
 		System.out.println("Please enter the number of times the loan is compounded.");
 		double number = kb.nextDouble();
 		double payment = calcC.calcPayment(rate, princpl, number, time);
-		System.out.println("Your monthly payment will be " + payment);
+		form.print(payment);
 		
 		
 	}
@@ -22,5 +23,10 @@ public class compound_interest
 	public static double calcPayment(double r, double p, double n, double t)
 	{
 		return (p * (Math.pow(1+r/n, n*t))/(12*t));
+	}
+	
+	public static void print(double payment)
+	{
+		System.out.printf("Your monthly payment will be %.2f ", payment);
 	}
 }
