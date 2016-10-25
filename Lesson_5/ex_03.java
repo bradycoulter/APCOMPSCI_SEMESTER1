8ac91ea42642eb677c90a1122f8d0b283619a707
bradycoulter <brady.coulter@gmail.com>
2016-10-25 14:27:01 -0700

    5 part 2

--- Lesson_5/ex_03.java
+++ Lesson_5/ex_03.java
@@ -1,2 +1,13 @@
 import java.util.Scanner;
-public class
+
+public class ex__03
+{
+	public static void main(String[]args)
+	{
+		ex_03 calc = new ex_03();
+		Scanner kb = new Scanner(System.in);
+		
+		
+		
+	}
+}
