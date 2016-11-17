a70bc76df044c4612c96668d69c7ece4bf754e2a
bradycoulter <brady.coulter@gmail.com>
2016-11-17 14:34:34 -0800

    lab 5 basically done

--- Lesson_5/ex_06.java
+++ Lesson_5/ex_06.java
@@ -2,25 +2,38 @@ import java.util.Scanner;
 
 public class ex_06
 {
-	static String userguess;
-	static String passguess;
+	static String userguess, passguess, username, password;
 	public static void main(String[]args)
 	{
-		String username = "user1";
-		String password = "pass123";
+		Scanner kb = new Scanner(System.in);
+		username = "user1";
+		password = "pass123";
 		System.out.println("Please enter your username: ");
-		String userguess = kb.nextLine();
+		userguess = kb.nextLine();
 		System.out.println("Please enter your password: ");
 		passguess = kb.nextLine();
 		passCheck(username, password);
 	}
 	public static void passCheck(String username, String password)
 	{
-		if (userguess.equals(username))
-			if (passguess.equals(password))
-				System.out.println("You are granted access!");
-			else
-				System.out.println("Your password is incorrect! Please try again");
+		if (userguess.equals(username) && passguess.equals(password))
+			System.out.println("You are granted access!");
+		else
+			if(userguess.equals(username))
+			{
+				System.out.println("Your password is incorrect! Please try again.");
 				passCheck(username, password);
+			}
+			else if(passguess.equals(password))
+			{
+				System.out.println("Your username is incorrect! Please try again.");
+				passCheck(username, password);
+			}
+			else
+			{
+				System.out.println("Your username and password are incorrect! Please try again.");
+				passCheck(username, password);
+			}
+
 	}
 }
